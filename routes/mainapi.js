d2ae4d31a444dfcff2b65f0fd7d38d6edcb115bb
wzwolf <wzwolfx@gmail.com>
2023-05-05 14:37:38 +0000

    added more description to api test, added post info

--- routes/mainapi.js
+++ routes/mainapi.js
@@ -3,16 +3,32 @@ const router = express.Router();
 const info = require('../models/mainModel');
 
 // get all entry
-router.get('/',(req,res) => {
-	res.send('get all entry');
+router.get('/', async (req,res) => {
+	//res.send('get all entry');
+	try {
+		const allInfo = await info.find();
+		res.json(allInfo);
+	} catch (err) {
+		res.status(500).json({message: err.message});
+	}
 });
 // get one entry
 router.get('/:id',(req,res) => {
 	res.send('get one entry');
 });
 // create one entry
-router.post('/',(req,res) => {
-	res.send('create entry');
+router.post('/',async (req,res) => {
+	//res.send('create entry');
+	const newInfo = new info({
+		name: req.body.name,
+		info: req.body.info
+	});
+	try {
+		const saveInfo = await newInfo.save();
+		res.status(201).json(saveInfo);
+	} catch(err) {
+		res.status(400).json({message:err.message});
+	}
 });
 // updating one entry
 router.patch('/',(req,res) => {
--- test/testapi.sh
+++ test/testapi.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
+echo "start restapi test"
+echo "test case 1: get all "
 curl "http://localhost:8080/main"
 echo -e "\n"
+
+echo "test case 2: Get id" 
 curl "http://localhost:8080/main/:id"
 echo -e "\n"
+
+echo "test case 3: POST nothing (should return err)"
 curl --request POST "http://localhost:8080/main"
 echo -e "\n"
+
+echo "test case 4: POST new info"
+curl --request POST "http://localhost:8080/main"
+
+echo -e "\n"
+
+echo "test case 5: Update info"
 curl --request PATCH "http://localhost:8080/main"
 echo -e "\n"
+
+echo "test case 6: delete info"
 curl --request DELETE "http://localhost:8080/main"
 echo -e "\n"
